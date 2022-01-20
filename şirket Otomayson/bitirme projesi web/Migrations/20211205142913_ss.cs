f455fcefed5de6cc39c7f43e9618234bf2c2eb63
ABDURRAHMAN YILDIRIM <57931170+adbdurrahman@users.noreply.github.com>
2022-01-20 18:52:35 +0300

    1

---
+++ şirket Otomayson/bitirme projesi web/Migrations/20211205142913_ss.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BitirmeProjesiWeb.Migrations
+{
+    public partial class ss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "FullName" },
+                values: new object[] { "noor@gmail.com", "NOOR NOOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "FullName" },
+                values: new object[] { "yazanalkhatib956@gmail.com", "Yazan ALKHATIB" });
+        }
+    }
+}
